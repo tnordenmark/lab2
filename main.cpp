ecfa4274669e20238f1b8e200066efe7b6ae29b3
Thomas Nordenmark <t.nordenmark@gmail.com>
2013-11-20 18:20:06 +0100

    Final application.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int counter = 0;
-    int count_tri = 0;
+    int a2 = 0; // Katet a i kvadrat
+    int b2 = 0; // Katet b i kvadrat
+    int c2 = 0; // Hypotenusa i kvadrat
+    int count = 0; // Räknare för antal pytagoreiska trianglar
 
-    for(int a = 1; a <= 500; a++)
+    for(int i = 1; i <= 500; i++) // Loop för att beräkna hypotenusan
     {
-        for(int b = 1; b <= 500; b++)
+        c2 = i * i; // hypotenusan i kvadrat
+        for(int j = 1; j <= 500; j++) // Loop för att beräkna katet b
         {
-            counter++;
-            c = sqrt((a * a)+(b * b));
-            if(c == 100 || c == 200 || c == 300 || c == 400 || c == 500)
-                cout << "Triangel med hypotenusan " << c >> ", katet a = " << a << ", katet b = " << b << endl;
-            if(((a*a)))
+            b2 = j * j; // Katet b i kvadrat
+            for(int k = 1; k < j; k++) // Loop för att beräkna katet a
+            {
+                a2 = k * k; // Katet a i kvadrat
+                if(c2 == a2 + b2)
+                {
+                    // Om hypotenusan är jämt hundratal, skriv ut information
+                    if(i == 100 || i == 200 || i == 300 || i == 400 || i == 500)
+                        cout << "Triangel med hypotenusan " << i << ", katet a = " << k << ", katet b = " << j << endl;
+                    count++; // Öka på räknaren för antal trianglar med 1 för varje varv
+                }
+            }
         }
     }
+
+    cout << "\nAntal pytagoreiska trianglar: " << count << endl;
 
     return 0;
 }
